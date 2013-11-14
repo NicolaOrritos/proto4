4fb39869b2553545fc3a896cad54739ca01af805
Nicola Orritos <nicola.orritos@gmail.com>
2013-11-14 10:26:08 +0100

    Added search by term API

--- proto.js
+++ proto.js
@@ -4,9 +4,22 @@ var xtr = require("./index.js");
 var examplePath = "examples/Cosmos.pdf";
 var docID = "Cosmos.pdf";
 
-xtr.actor.load(examplePath, docID, function()
+/* xtr.actor.load(examplePath, docID, function()
 {
     console.log("Document was processed");
-});
+}); */
 
+xtr.actor.search("re*", function(result)
+{
+    console.log("Document was searched");
+    console.log("Result: %s", result);
+    
+    if (result)
+    {
+        for (res in result)
+        {
+            console.log("Res: %s", res);
+        }
+    }
+});
 
