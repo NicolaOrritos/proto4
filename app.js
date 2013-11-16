6c0dd0d0bf338f245f168cff4e56fe974d37f797
Nicola Orritos <nicola.orritos@gmail.com>
2013-11-16 18:14:35 +0100

    Completely reworked search

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 
 var xtr = require("./index.js");
 
-/* var examplePath = "examples/Cosmos.pdf";
-var docID = "Cosmos.pdf";
-
-xtr.actor.load(examplePath, docID, function()
+xtr.actor.init(function(error, result)
 {
-    console.log("Document was processed");
-}); */
-
-xtr.actor.search("re*", function(result)
-{
-    console.log("Document was searched");
-    console.log("Result: %s", result);
+    // Result is the set of docs of this xtr.actor instance
+    console.log("docs: %s", result);
     
-    if (result)
+    /* var examplePath = "examples/Cosmos.pdf";
+    var docID = "Cosmos.pdf";
+
+    xtr.actor.load(examplePath, docID, function()
     {
-        for (res in result)
+        console.log("Document was processed");
+    }); */
+    
+    
+    var start = new Date();
+
+    xtr.actor.search("result", function(result)
+    {
+        console.log("Document was searched");
+        
+        var end = new Date();
+        
+        var time = end - start;
+        
+        console.log("\nmilliseconds: #%d", time);
+        console.log("seconds:      #%d", (time / 1000));
+        
+        
+        if (result)
         {
-            console.log("Res: %s", res);
+            for (var a=0; a<result.length; a++)
+            {
+                console.log("Res: %s", result[a]);
+            }
         }
-    }
+    });
+    
 });
 
